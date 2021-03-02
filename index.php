0b9d08f9c7b26185a590f0128936c64118b214ad
Carlos Andres Valero <carlosandresvalerovelasco@gmail.com>
2021-03-02 13:53:07 -0500

    inicio tablas

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>CoolStyle</h1>
+    <a href="producto/producto.php">PRODUCTO</a><br>
+    <a href="envio/envio.php">ENVIO</a><br>
+    <a href="proveedor/proveedor.php">PROVEEDOR</a><br>
+    <a href="pedido_p/pedido_p.php">PEDIDO</a><br>
+    <a href="reclamos/reclamos.php">RECLAMOS</a>
+</body>
+</html>
