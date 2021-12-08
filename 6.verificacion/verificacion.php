e8264704347a114a237c3af8d2cbcc775b4c5d0b
Carlos Andres Valero <carlosandresvalerovelasco@gmail.com>
2021-12-08 13:28:46 -0500

    logout y verificacion

--- 6.verificacion/verificacion.php
+++ 6.verificacion/verificacion.php
@@ -8,17 +8,13 @@ $records->execute();
 $results = $records->fetch(PDO::FETCH_ASSOC);
 
 if(password_verify($_POST['pass'], $results['pass']) && $results['codigo_c']==1){
-
     $_SESSION['admin'] = $results;
     //echo "cuenta admin <br>";
     header('Location: ../1.menu/indice.php');
-
 }elseif(password_verify($_POST['pass'], $results['pass']) && $results['codigo_c']!=1){
-    
     $_SESSION['cliente'] = $results; 
     //echo "cuenta user <br>";
     header('Location: ../index.php');
-
 }else{
     echo "error";
 }
