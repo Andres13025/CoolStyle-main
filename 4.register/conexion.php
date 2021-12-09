1d959818a6cfbf0dd631b16648f291b860802668
Carlos Andres Valero <carlosandresvalerovelasco@gmail.com>
2021-12-08 19:19:16 -0500

    change

--- 4.register/conexion.php
+++ 4.register/conexion.php
@@ -2,7 +2,7 @@
     $server = 'btibuy9uvhaujjwxul6o-mysql.services.clever-cloud.com';
     $database = 'btibuy9uvhaujjwxul6o';
     $usuario = 'uvk3cic7kntjqwio';
-    $password = 'jUsHhVPat7EFnHyBwOOc';
+    $password = 'CrcLHSIuGW6tDsUJ5fPQ';
     
     try{
         $conn = new PDO("mysql:host=$server;dbname=$database;",$usuario, $password);
--- 4.register/recibir_registrarse.php
+++ 4.register/recibir_registrarse.php
@@ -1,33 +1,33 @@
 <?php
-    // require_once 'conexion.php';
+    require_once 'conexion.php';
 
-    // $mensaje='';
-
-    // if (!empty($_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['pass'])){
-    //     $sql = "INSERT INTO cliente (nombre, usuario, pass) VALUES (:nombre, :usuario, :pass)";
-    //     $stmt = $conn->prepare($sql);
-    //     $stmt->bindParam(':nombre',$_POST['nombre']);
-    //     $stmt->bindParam(':usuario',$_POST['usuario']);
-    //     $password = password_hash($_POST['pass'], PASSWORD_BCRYPT);
-    //     $stmt->bindParam(':pass', $password);
-
-    //     if($stmt->execute()){
-    //         /*$mensaje = 'Successfully created new user';*/
-    //         header('Location: ../3.login/login.php');
-    //     }else{
-    //         $mensaje = 'Sorry there must have been an issue creating your password';
-    //     }
-
-    // }
-    // echo $mensaje;
-
-    
-    include_once "../controller/crud.php";
-    $modelo_r = new administrar();
+    $mensaje='';
 
     if (!empty($_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['pass'])){
-        $resultado = $modelo_r->agregar_usuarionuevo($_POST['nombre'], $_POST['usuario'], $_POST['pass']);
-    }else{
-        echo "error";
+        $sql = "INSERT INTO cliente (nombre, usuario, pass) VALUES (:nombre, :usuario, :pass)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nombre',$_POST['nombre']);
+        $stmt->bindParam(':usuario',$_POST['usuario']);
+        $password = password_hash($_POST['pass'], PASSWORD_BCRYPT);
+        $stmt->bindParam(':pass', $password);
+
+        if($stmt->execute()){
+            /*$mensaje = 'Successfully created new user';*/
+            header('Location: ../3.login/login.php');
+        }else{
+            $mensaje = 'Sorry there must have been an issue creating your password';
+        }
+
     }
+    echo $mensaje;
+
+    
+    // include_once "../controller/crud.php";
+    // $modelo_r = new administrar();
+
+    // if (!empty($_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['pass'])){
+    //     $resultado = $modelo_r->agregar_usuarionuevo($_POST['nombre'], $_POST['usuario'], $_POST['pass']);
+    // }else{
+    //     echo "error";
+    // }
 ?>
