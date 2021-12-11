4236f04690bd3fcff769bbd8845dc936289a0a09
Carlos Andres Valero <carlosandresvalerovelasco@gmail.com>
2021-12-11 13:48:34 -0500

    estilos admin try 1

--- 1.menu/indice.php
+++ 1.menu/indice.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel=”stylesheet” href=”https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css” rel=”nofollow” integrity=”sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I” crossorigin=”anonymous”>
     <script src=”https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js” integrity=”sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/” crossorigin=”anonymous”></script>
-    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <!-- <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script> -->
     <link href="../fontawesome/css/all.css" rel="stylesheet"> 
 </head>
 <body>
@@ -70,12 +70,9 @@
 </center>
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" hidden>Open modal for @mdo</button>
 
-
-
-
 </body>
 </html>
-<script>
+<!-- <script>
     $(document).ready(function () {
         $("#button").click();
         $("iframe1").attr("src","indice.php");
@@ -96,5 +93,5 @@
         });
     });
     
-</script>
+</script> -->
 
