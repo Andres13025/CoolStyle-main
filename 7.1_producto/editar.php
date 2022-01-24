7cf901c59b9caff9074ade903492de47e67189b8
Carlos Andres Valero <carlosandresvalerovelasco@gmail.com>
2022-01-24 15:05:47 -0500

    5

--- 7.1_producto/editar.php
+++ 7.1_producto/editar.php
@@ -4,14 +4,9 @@
     $modelo_o = new administrar();
     $modelo_e = new administrar();
     $datos = $modelo_o->ObtenerDatos($id);//este solo trae los datos que seran editados
+   
 
-    print_r($datos);
-    echo $datos['identificacion'];
-    echo '<script language="javascript">console.log("hola")</script>';
-
-    
-
-    echo "<form action='editar.php' method='post'>
+    echo "<form action='pruebaeditar.php' method='post'>
     <table>
         <tr>
             <td>Codigo</td>
@@ -41,37 +36,37 @@
     </table>
 </form>";
 
-if(isset($_POST['Submit'])){
+// if(isset($_POST['Submit'])){
     
-    $modelo_asd = new administrar();
-    $id = $_POST['oculto'];
-    $codigo = $_POST['codigo'];
-    $nombre = $_POST['nombre'];
-    $cantidad = $_POST['cantidad'];
-    $precio = $_POST['precio'];
-    $talla = $_POST['talla'];
+//     $modelo_asd = new administrar();
+//     $id = $_POST['oculto'];
+//     $codigo = $_POST['codigo'];
+//     $nombre = $_POST['nombre'];
+//     $cantidad = $_POST['cantidad'];
+//     $precio = $_POST['precio'];
+//     $talla = $_POST['talla'];
 
 
-    echo '<script language="javascript">console.log("hola")</script>';
-    echo '<script language="javascript">console.log('.$id.')</script>';
-    echo '<script language="javascript">console.log('.$codigo.')</script>';
-    echo '<script language="javascript">console.log('.$nombre.')</script>';
-    echo '<script language="javascript">console.log('.$cantidad.')</script>';
-    echo '<script language="javascript">console.log('.$precio.')</script>';
-    echo '<script language="javascript">console.log('.$talla.')</script>';
+//     echo '<script language="javascript">console.log("hola")</script>';
+//     echo '<script language="javascript">console.log('.$id.')</script>';
+//     echo '<script language="javascript">console.log('.$codigo.')</script>';
+//     echo '<script language="javascript">console.log('.$nombre.')</script>';
+//     echo '<script language="javascript">console.log('.$cantidad.')</script>';
+//     echo '<script language="javascript">console.log('.$precio.')</script>';
+//     echo '<script language="javascript">console.log('.$talla.')</script>';
     
 
-    try {
-        $modelo_asd->editar($id, $codigo, $nombre, $cantidad, $precio, $talla);
-    } catch (Exception $th) {
-        echo $th->getMessage();
-        echo '<script language="javascript">alert('.$th->getMessage().');</script>';
-    }
+//     try {
+//         $modelo_asd->editar($id, $codigo, $nombre, $cantidad, $precio, $talla);
+//     } catch (Exception $th) {
+//         echo $th->getMessage();
+//         echo '<script language="javascript">alert('.$th->getMessage().');</script>';
+//     }
     
     
     
-}else{
-    echo "no enviado";
-}
+// }else{
+//     echo "no enviado";
+// }
 
 ?>
