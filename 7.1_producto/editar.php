f305b7c3f96df5c557ae29931ff2cf7f709ca244
Carlos Andres Valero <carlosandresvalerovelasco@gmail.com>
2022-01-24 14:30:26 -0500

    ajuste1

--- 7.1_producto/editar.php
+++ 7.1_producto/editar.php
@@ -42,14 +42,21 @@
 
 if(isset($_POST['Submit'])){
     
+    $modelo_asd = new administrar();
     $id = $_POST['oculto'];
     $codigo = $_POST['codigo'];
     $nombre = $_POST['nombre'];
     $cantidad = $_POST['cantidad'];
     $precio = $_POST['precio'];
     $talla = $_POST['talla'];
+
+    try {
+        $modelo_asd->editar($id, $codigo, $nombre, $cantidad, $precio, $talla);
+    } catch (Exception $th) {
+        echo $th->getMessage();
+    }
     
-    $resultado = $modelo_e->editar($id, $codigo, $nombre, $cantidad, $precio, $talla);
+    
     
 }else{
     echo "no enviado";
